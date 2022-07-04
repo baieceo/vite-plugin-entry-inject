866733193781f3537ed403010d1e2d3940444252
chenxi5 <chenxi5@01zhuanche.com>
2022-07-04 15:24:15 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # vite-plugin-entry-inject
+
+修改打包后入口文件在html中的注入位置
+
+### 使用说明
+```sh
+npm install --save-dev vite-plugin-entry-inject
+# or
+yarn add vite-plugin-entry-inject -D
+```
+
+```js
+// vite.config.js
+import VitePluginEntryInject from 'vite-plugin-entry-inject';
+
+export default {
+    plugins: [{
+        VitePluginEntryInject({
+            // head-prepend/head/body-prepend/body
+            injectTo: 'body'
+        })
+    }]
+};
+```
+
+## 参数
+
+### `injectTo`
+入口文件注入位置，默认`body`，即body闭合标签`</body>`前，可选。
+#### `head-prepend`: `<head>`开始标签后
+#### `head`: `</head>`闭合标签前
+#### `body-prepend`: `<body>`开始标签后
+#### `body`: `</body>`闭合标签前
+
+- **Type:** `string`
+- **Default:** `body`
---
+++ src/index.js
@@ -0,0 +1,43 @@
+function vitePluginEntryInject(config) {
+    return {
+        name: 'rollup-plugin-entry-inject',
+        transformIndexHtml(html, ctx) {
+            const { injectTo = 'body' } = config;
+
+            let script = '';
+
+            if (ctx.chunk.isEntry) {
+                html = html.replace(new RegExp('<script[^<>]+' + ctx.chunk.fileName + '[^<>]+><\/script>'), match => {
+                    script = match;
+
+                    return '';
+                });
+
+                const injectMap = {
+                    'head': {
+                        tag: '</head>',
+                        template: `${script}</head>`,
+                    },
+                    'head-prepend': {
+                        tag: '<head>',
+                        template: `<head>${script}`,
+                    },
+                    'body': {
+                        tag: '</body>',
+                        template: `${script}</body>`,
+                    },
+                    'body-prepend': {
+                        tag: '<body>',
+                        template: `<body>${script}`,
+                    }
+                };
+
+                html = html.replace(injectMap[inject].tag, injectMap[inject].template);
+            }
+
+            return html;
+        }
+    };
+}
+
+module.exports = vitePluginEntryInject;
